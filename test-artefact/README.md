d6a5ee197ee0c16709fb0b37618821f381ea9c83
Juraj Veverka <gergej123@gmail.com>
2020-12-30 11:18:43 +0100

    created test project

---
+++ test-artefact/README.md
@@ -0,0 +1,1 @@
+# Test artefact publishing
---
+++ test-artefact/src/main/java/one/microproject/test/TestUtils.java
@@ -0,0 +1,12 @@
+package one.microproject.test;
+
+public class TestUtils {
+
+    private TestUtils() {
+    }
+
+    private String getMessage() {
+        return "hello world !";
+    }
+
+}
