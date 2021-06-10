5388638a46b6659d9faede48645a3a1812f654c6
Juraj Veverka <gergej123@gmail.com>
2021-06-10 22:47:37 +0200

    dockerfile refactoring

--- event-logger-service/event-logger-start.sh
+++ event-logger-service/event-logger-start.sh
@@ -7,11 +7,11 @@ if [ "${APP_CONFIG_PATH}" = "false" ]; then
   echo "using default configuration"
   echo "SERVER_PORT=${SERVER_PORT}"
   echo "XMX=${XMX}"
-  java -Xms32m -Xmx${XMX} -jar /event-logger-service-0.0.1-SNAPSHOT.jar
+  java -Xms32m -Xmx${XMX} -jar /event-logger-service.jar
 else
   echo "using custom configuration"
   echo "APP_CONFIG_PATH=${APP_CONFIG_PATH}"
   echo "XMX=${XMX}"
-  java -Xms32m -Xmx${XMX} -jar /event-logger-service-0.0.1-SNAPSHOT.jar \
+  java -Xms32m -Xmx${XMX} -jar /event-logger-service.jar \
      --spring.config.location=file:${APP_CONFIG_PATH}
 fi
