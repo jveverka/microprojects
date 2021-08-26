3b999d04b5d0627bc6456a3e15579a2a5e6e07cc
Juraj Veverka <gergej123@gmail.com>
2021-08-27 00:45:16 +0200

    implementing netcat waiting

--- event-logger-service/event-logger-start.sh
+++ event-logger-service/event-logger-start.sh
@@ -3,6 +3,13 @@
 echo "starting event-logger ..."
 echo "APP_CONFIG_PATH=${APP_CONFIG_PATH}"
 
+echo "Waiting for mongodb to launch on 27017..."
+while ! nc -z mongodb 27017; do
+  sleep 0.5 # wait for 0.5s before check again
+  echo -n "."
+done
+echo "MongoDB launched !"
+
 if [ "${APP_CONFIG_PATH}" = "false" ]; then
   echo "using default configuration"
   echo "SERVER_PORT=${SERVER_PORT}"
