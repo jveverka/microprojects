97861c66f90f429412a1f6fc65de1a2226e369d4
Juraj Veverka <gergej123@gmail.com>
2021-10-17 19:44:21 +0200

    config fixed

--- events/src/main/java/one/microproject/events/config/RestClientConfig.java
+++ events/src/main/java/one/microproject/events/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package one.microproject.events.config;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomCo
 
 import java.util.Arrays;
 
+@Configuration
 public class RestClientConfig extends AbstractElasticsearchConfiguration {
 
     @Value("${app.elastic-host}")
