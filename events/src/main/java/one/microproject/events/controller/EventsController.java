3cc346dddcb6aad9b115347b12c08b5e5a1deaf4
Juraj Veverka <gergej123@gmail.com>
2021-12-04 00:12:53 +0100

    code refactoring

--- events/src/main/java/one/microproject/events/controller/EventsController.java
+++ events/src/main/java/one/microproject/events/controller/EventsController.java
@@ -3,52 +3,35 @@ package one.microproject.events.controller;
 import one.microproject.events.dto.EventDTO;
 import one.microproject.events.dto.IdDTO;
 import one.microproject.events.model.EventDocument;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
+import one.microproject.events.service.EventLoggerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/events")
 public class EventsController {
 
-    private ElasticsearchOperations elasticsearchOperations;
+    private EventLoggerService eventLoggerService;
 
-    public EventsController(ElasticsearchOperations elasticsearchOperations) {
-        this.elasticsearchOperations = elasticsearchOperations;
+    @Autowired
+    public EventsController(EventLoggerService eventLoggerService) {
+        this.eventLoggerService = eventLoggerService;
     }
 
     @PostMapping()
     public IdDTO save(@RequestBody EventDTO eventDTO) {
-        String id = UUID.randomUUID().toString();
-        Date date = new Date();
-        EventDocument eventDocument = new EventDocument(id, date, "user-001",
-                eventDTO.streamId(), eventDTO.activityType(), eventDTO.quantity(), eventDTO.duration(), eventDTO.unit(), eventDTO.labels());
-        IndexQuery indexQuery = new IndexQueryBuilder()
-                .withId(id)
-                .withObject(eventDocument)
-                .build();
-        String documentId = elasticsearchOperations.index(indexQuery, IndexCoordinates.of("events"));
-        return new IdDTO(documentId);
+        return eventLoggerService.save("user-001", eventDTO);
     }
 
     @GetMapping()
     public List<EventDocument> getAll() {
-        SearchHits<EventDocument> events = elasticsearchOperations
-                .search(Query.findAll(), EventDocument.class);
-        return events.get().map(s->s.getContent()).collect(Collectors.toList());
+        return eventLoggerService.getAll();
     }
 
 }
--- events/src/main/java/one/microproject/events/service/EventLoggerService.java
+++ events/src/main/java/one/microproject/events/service/EventLoggerService.java
@@ -1,4 +1,15 @@
 package one.microproject.events.service;
 
+import one.microproject.events.dto.EventDTO;
+import one.microproject.events.dto.IdDTO;
+import one.microproject.events.model.EventDocument;
+
+import java.util.List;
+
 public interface EventLoggerService {
+
+    IdDTO save(String userId, EventDTO eventDTO);
+
+    List<EventDocument> getAll();
+
 }
