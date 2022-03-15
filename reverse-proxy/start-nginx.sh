6d9e2c939694807a3f43cb0e0df91719ac734d4d
Juraj Veverka <gergej123@gmail.com>
2022-03-15 21:02:24 +0100

    adding reverse proxy

--- reverse-proxy/start-nginx.sh
+++ reverse-proxy/start-nginx.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-CONFIG_URL=${CS_BASE_URL}/application/default/uat/reverse-proxy/nginx.conf
-CERT_BASE_URL=${CS_BASE_URL}/application/default/uat/reverse-proxy
+echo "USE_TLS        =${USE_TLS}"
+echo "CS_SERVICE_NAME=${CS_SERVICE_NAME}"
+echo "CS_APPLICATION =${CS_APPLICATION}"
+echo "CS_PROFILE     =${CS_PROFILE}"
+echo "CS_LABEL       =${CS_LABEL}"
+
+CONFIG_URL=${CS_BASE_URL}/${CS_APPLICATION}/${CS_PROFILE}/${CS_LABEL}/${CS_SERVICE_NAME}/nginx.conf
+CERT_BASE_URL=${CS_BASE_URL}/${CS_APPLICATION}/${CS_PROFILE}/${CS_LABEL}/${CS_SERVICE_NAME}
 
 rm /etc/nginx/http.d/default.conf
 echo "Downloading NGINX config from: ${CONFIG_URL}"
@@ -9,13 +15,13 @@ curl -sS --user ${CS_USERNAME}:${CS_PASSWORD} ${CONFIG_URL} -o /etc/nginx/http.d
 echo "CURL: $?"
 
 if [ ${USE_TLS} = "true" ]; then
-  echo "Downloading server certs from: ${CERT_BASE_URL}"
-  curl -sS --user ${CS_USERNAME}:${CS_PASSWORD} ${CERT_BASE_URL}/server.cer -o /etc/nginx/http.d/server.cer
-  echo "CURL: $?"
-  curl -sS --user ${CS_USERNAME}:${CS_PASSWORD} ${CERT_BASE_URL}/server.key -o /etc/nginx/http.d/server.key
-  echo "CURL: $?"
+   echo "Downloading server certs from: ${CERT_BASE_URL}"
+   curl -sS --user ${CS_USERNAME}:${CS_PASSWORD} ${CERT_BASE_URL}/server.cer -o /etc/nginx/http.d/server.cer
+   echo "CURL: $?"
+   curl -sS --user ${CS_USERNAME}:${CS_PASSWORD} ${CERT_BASE_URL}/server.key -o /etc/nginx/http.d/server.key
+   echo "CURL: $?"
 else
-   echo "USE_TLS = ${USE_TLS}"
+   echo "Downloading of server certs skipped."
 fi
 
 #/usr/sbin/nginx -g 'daemon off;'
