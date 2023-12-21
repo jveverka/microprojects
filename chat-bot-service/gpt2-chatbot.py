460e30803681148d10519d13c0c8b2d217df9ea3
Juraj Veverka <gergej123@gmail.com>
2023-12-21 13:47:19 +0100

    adding simple chatbot

---
+++ chat-bot-service/gpt2-chatbot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+def load_model(model_name="gpt2"):
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    return model, tokenizer
+
+def generate_text(prompt, model, tokenizer):
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+model, tokenizer = load_model()
+
+while True:
+    print("Welcome to the GPT-based chatbot. Type 'exit' to end the conversation.")
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        print("Chatbot: Goodbye!")
+        break
+    print(generate_text(user_input, model, tokenizer))
+
